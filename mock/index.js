1074edceb9e8b81cb0c6684608acb79620c74dae
Muhamad Zulfikri <muhzulfik13@gmail.com>
2022-06-15 16:17:24 +0700

    add data dummy

---
+++ mock/index.js
@@ -0,0 +1,16 @@
+export const user = {
+  nama_lengkap: "Zaki",
+  tanggal_lahir: "2008-11-11",
+  nik: "99976663331",
+  email: "kuepukis@gmail.com",
+  no_hp: "08588273",
+};
+
+export const patient = {
+  rumah_sakit: "karya medika",
+  pasien: {
+    pasien_id: "123",
+    nama_lengkap: "Zaki",
+    nik: "999766633312",
+  },
+};
